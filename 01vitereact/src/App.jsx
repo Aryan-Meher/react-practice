4d12d8edee0878a106189bff81168e566464f820
Aryan Meher <aryanmeher4242@gmail.com>
2026-07-10 16:56:53 +0530

    Add custom rendering logic and HTML structure for the custom React app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,12 @@ import Hello from './hello'
 
 
 function App() {
-  
+  const username = "Aryan"
 
   return (
     <>
     <Hello/>
-    <h1>hello react</h1>
+    <h1>hello react {username}</h1>
     <p>test para</p>
     </>
   )
